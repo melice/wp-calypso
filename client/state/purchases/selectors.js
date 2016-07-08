fd795a3867513591918c138103d18da4f8712024
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-07-08 16:25:36 -0400

    Purchases: Use `state.purchases.items` in `PurchasesList`

--- client/state/purchases/selectors.js
+++ client/state/purchases/selectors.js
@@ -6,6 +6,10 @@
  */
 export const getPurchases = state => state.purchases.items.data;
 
+export const getUserPurchases = ( state, userId ) => (
+	state.purchases.items.hasLoadedUserPurchasesFromServer && getPurchases( state ).filter( purchase => purchase.userId === userId )
+);
+
 /**
  * Returns a Purchase object from the state using its id
  * @param  {Object} state       global state
@@ -16,5 +20,6 @@ export const getByPurchaseId = ( state, purchaseId ) => (
 	getPurchases( state ).filter( purchase => purchase.id === purchaseId ).shift()
 );
 
+export const hasLoadedUserPurchasesFromServer = state => state.purchases.items.hasLoadedUserPurchasesFromServer;
 export const isFetchingUserPurchases = state => state.purchases.items.isFetchingUserPurchases;
 export const isFetchingSitePurchases = state => state.purchases.items.isFetchingSitePurchases;
